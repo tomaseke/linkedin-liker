2ba4679a96681223ef4a6e1d9ad82fb8de7cf8a8
herby <tomasekerbenu@gmail.com>
2022-07-25 23:06:51 +0200

    fix21

--- index.js
+++ index.js
@@ -7,57 +7,61 @@ require("dotenv").config();
 
 app.get("/", async (req, res) => {
   like();
-  res.send('hi');
+  res.send("hi");
 });
 
 app.listen(PORT, () => {
-   console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}`);
 });
 
 async function like() {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
     headless: false,
   });
-  try{
-  const page = await browser.newPage();
-  await page.goto("https://linkedin.com");
-  await page.click(
-    'a[data-tracking-control-name="guest_homepage-basic_nav-header-signin"]'
-  );
-  await page.type("#username", "tomasekerbenu@gmail.com", {delay: 50});
-  await page.type("#password", process.env.password, {delay: 50});
-  await page.click("button[type=submit]");
-  await page.waitForTimeout(6000);
-  await page.waitForSelector(".search-global-typeahead__collapsed-search-button-icon");
-  await page.click(".search-global-typeahead__collapsed-search-button-icon");
-  await page.type(".search-global-typeahead__input", 'decisionrules.io');
-  await page.focus(".search-global-typeahead__collapsed-search-button-icon");
-  await page.waitForTimeout(6000);
-  await page.keyboard.press("ArrowDown");
-  await page.keyboard.press("Enter");
-  await page.waitForSelector(".image-text-lockup__text");
-  await page.click(".image-text-lockup__text");
-  await page.waitForSelector('a[href="/company/decisionrules-io/posts/"]');
-  await page.click('a[href="/company/decisionrules-io/posts/"]');
-  await page.waitForSelector('button[data-control-name="feed_sort_dropdown_trigger"]');
-  await page.click('button[data-control-name="feed_sort_dropdown_trigger"]');
-  await page.waitForTimeout(2000);
-  await page.keyboard.press("ArrowDown");
-  await page.keyboard.press("ArrowDown");
-  await page.waitForTimeout(2000);
-  await page.keyboard.press("Enter");
-  await page.waitForTimeout(6000);
-  await page.evaluate(async () => {
-    const allLikeButtons = document.querySelectorAll('button[aria-pressed=false].artdeco-button--tertiary');
-    allLikeButtons.forEach(btn => btn.click());
-  })
-  console.log('worked');
-  }
-  catch(e) {
+  try {
+    const page = await browser.newPage();
+    await page.goto("https://linkedin.com");
+    await page.click(
+      'a[data-tracking-control-name="guest_homepage-basic_nav-header-signin"]'
+    );
+    await page.type("#username", "tomasekerbenu@gmail.com", { delay: 50 });
+    await page.type("#password", process.env.password, { delay: 50 });
+    await page.click("button[type=submit]");
+    await page.waitForTimeout(6000);
+    await page.waitForSelector(
+      ".search-global-typeahead__collapsed-search-button-icon"
+    );
+    await page.click(".search-global-typeahead__collapsed-search-button-icon");
+    await page.type(".search-global-typeahead__input", "decisionrules.io");
+    await page.focus(".search-global-typeahead__collapsed-search-button-icon");
+    await page.waitForTimeout(6000);
+    await page.keyboard.press("ArrowDown");
+    await page.keyboard.press("Enter");
+    await page.waitForSelector(".image-text-lockup__text");
+    await page.click(".image-text-lockup__text");
+    await page.waitForSelector('a[href="/company/decisionrules-io/posts/"]');
+    await page.click('a[href="/company/decisionrules-io/posts/"]');
+    await page.waitForSelector(
+      'button[data-control-name="feed_sort_dropdown_trigger"]'
+    );
+    await page.click('button[data-control-name="feed_sort_dropdown_trigger"]');
+    await page.waitForTimeout(2000);
+    await page.keyboard.press("ArrowDown");
+    await page.keyboard.press("ArrowDown");
+    await page.waitForTimeout(2000);
+    await page.keyboard.press("Enter");
+    await page.waitForTimeout(6000);
+    await page.evaluate(async () => {
+      const allLikeButtons = document.querySelectorAll(
+        "button[aria-pressed=false].artdeco-button--tertiary"
+      );
+      allLikeButtons.forEach((btn) => btn.click());
+    });
+    console.log("worked");
+  } catch (e) {
     console.error(e);
+  } finally {
+    await browser.close();
   }
-  finally {
-  await browser.close();
-  }
-};
+}
